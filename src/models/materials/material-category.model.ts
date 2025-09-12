127edd5c6c9d94d42ae700e8b4b008967b4e4650
Rajib Mondal <rajib.3t@gmail.com>
2025-09-13 03:47:20 +0530

    material category modal update

--- src/models/materials/material-category.model.ts
+++ src/models/materials/material-category.model.ts
@@ -1,8 +1,9 @@
 import { Schema, model, Document } from "mongoose";
-
+import { IUser } from "../user.model";
 export interface IMaterialCategory extends Document{
     name: string;
     code: string;
+    createdBy?: IUser["_id"];
 }
 
 
@@ -10,6 +11,7 @@ const MaterialCategorySchema : Schema = new Schema(
     {
         name: { type: String, require:true},
         code: { type: String, require:true, unique:true },
+        createdBy: { type: Schema.Types.ObjectId, ref: "User" , required: true},
     },
     {
         timestamps: true,
--- src/services/materials/material-category.service.ts
+++ src/services/materials/material-category.service.ts
@@ -1,3 +1,33 @@
+import { Connection } from "mongoose";
+import MaterialCategoryRepository from "../../repositories/materials/material-category.repository";
+import { IMaterialCategory } from "../../models/materials/material-category.model";
+
 class MaterialCategoryService {
     private static instance: MaterialCategoryService;
+
+    public static getInstance(): MaterialCategoryService {
+        if (!MaterialCategoryService.instance) {
+            MaterialCategoryService.instance = new MaterialCategoryService();
+        }
+        return MaterialCategoryService.instance;
+    }
+
+
+     // Original methods (for backward compatibility with main database)
+        public async create(userData: Partial<IMaterialCategory>): Promise<IMaterialCategory>;
+        public async create(tenantConnection: Connection, userData: Partial<IMaterialCategory>): Promise<IMaterialCategory>;
+        public async create(connectionOrData: Connection | Partial<IMaterialCategory>, userData?: Partial<IMaterialCategory>): Promise<IMaterialCategory> {
+            if (connectionOrData instanceof Connection) {
+                // Using tenant connection
+                const repository = new MaterialCategoryRepository(connectionOrData);
+                return repository.create(userData!);
+            } else {
+                // Using main database (backward compatibility)
+                const repository = new MaterialCategoryRepository();
+                return repository.create(connectionOrData);
+            }
+        }
 }
+
+
+export default MaterialCategoryService;
